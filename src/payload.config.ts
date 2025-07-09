5b8c2d1905c87d13fdaf7df6b5f459c0efcc3e8b
Justin Arnold <hello@justin-arnold.com>
2025-07-09 02:03:22 -0400

    feat: Allow all cors

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -65,7 +65,7 @@ export default buildConfig({
     },
   }),
   collections: [Pages, Posts, Media, Categories, Users],
-  cors: [getServerSideURL()].filter(Boolean),
+  cors: '*',
   globals: [Header, Footer],
   plugins: [
     ...plugins,
